e4e57cdd1e2dc6d1e734ea1a75bb81a22b88ae10
Brian Greenacre <bgreenacre42@gmail.com>
2021-11-11 21:11:11 -0600

    Use regexs to figure out keypresses

--- src/Keys.php
+++ src/Keys.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PhpTui\PhpTui;
 
-use Clue\React\Term\ControlCodeParser;
 use Clue\React\Utf8\Sequencer as Utf8Sequencer;
 use Evenement\EventEmitter;
 use Evenement\EventEmitterInterface;
@@ -13,6 +12,18 @@ use Symfony\Component\String\UnicodeString;
 
 class Keys extends EventEmitter
 {
+    const IS_MOUSE = [
+        '/\x1b\[M/',
+        '/\x1b\[M([\x00\u0020-\uffff]{3})/',
+        '/\x1b\[(\d+;\d+;\d+)M/',
+        '/\x1b\[<(\d+;\d+;\d+)([mM])/',
+        '/\x1b\[<(\d+;\d+;\d+;\d+)&w/',
+        '/\x1b\[24([0135])~\[(\d+),(\d+)\]\r/',
+        '/\x1b\[(O|I)/',
+    ];
+
+    const META_KEYCODE = '(?:\x1b)([a-zA-Z0-9])';
+    const FUNC_KEYCODE = '(?:\x1b+)(O|N|\\[|\\[\\[)(?:(\\d+)(?:;(\\d+))?([~^$])|(?:M([@ #!a`])(.)(.))|(?:1;)?(\\d+)?([a-zA-Z]))';
     const KEY_UP = 'up';
     const KEY_DOWN = 'down';
     const KEY_LEFT = 'left';
@@ -43,131 +54,206 @@ class Keys extends EventEmitter
     const KEY_F12 = 'f12';
 
     const CODES = [
-        "\033[A" => self::KEY_UP,
-        "\033[B" => self::KEY_DOWN,
-        "\033[C" => self::KEY_RIGHT,
-        "\033[D" => self::KEY_LEFT,
-        "\033[E" => self::KEY_CLEAR,
-        "\033[F" => self::KEY_END,
-        "\033[G" => self::KEY_HOME,
-        "\033OA" => self::KEY_UP,
-        "\033OB" => self::KEY_DOWN,
-        "\033OC" => self::KEY_RIGHT,
-        "\033OD" => self::KEY_LEFT,
-        "\033OE" => self::KEY_CLEAR,
-        "\033OF" => self::KEY_END,
-        "\033OG" => self::KEY_HOME,
+        "[A" => self::KEY_UP,
+        "[B" => self::KEY_DOWN,
+        "[C" => self::KEY_RIGHT,
+        "[D" => self::KEY_LEFT,
+        "[E" => self::KEY_CLEAR,
+        "[F" => self::KEY_END,
+        "[H" => self::KEY_HOME,
+        "OA" => self::KEY_UP,
+        "OB" => self::KEY_DOWN,
+        "OC" => self::KEY_RIGHT,
+        "OD" => self::KEY_LEFT,
+        "OE" => self::KEY_CLEAR,
+        "OF" => self::KEY_END,
+        "OH" => self::KEY_HOME,
         "\n" => self::KEY_ENTER,
         "\t" => self::KEY_TAB,
         "\x1b" => self::KEY_ESCAPE,
         "\x1b\x1b" => self::KEY_ESCAPE,
         "\x7f" => self::KEY_BACKSPACE,
         "\x04" => self::KEY_TERMINATE,
-        "\033[1~" => self::KEY_HOME,
-        "\033[2~" => self::KEY_INSERT,
-        "\033[3~" => self::KEY_DEL,
-        "\033[4~" => self::KEY_END,
-        "\033[5~" => self::KEY_PGUP,
-        "\033[6~" => self::KEY_PGDOWN,
-        "\033[[5~" => self::KEY_PGUP,
-        "\033[[6~" => self::KEY_PGDOWN,
-        "\033[7~" => self::KEY_HOME,
-        "\033[8~" => self::KEY_END,
-        "\033[OP"  => self::KEY_F1,
-        "\033[OQ"  => self::KEY_F2,
-        "\033[OR"  => self::KEY_F3,
-        "\033[OS"  => self::KEY_F4,
-        "\033[11~"  => self::KEY_F1,
-        "\033[12~"  => self::KEY_F2,
-        "\033[13~"  => self::KEY_F3,
-        "\033[14~"  => self::KEY_F4,
-        "\033[[A"  => self::KEY_F1,
-        "\033[[B"  => self::KEY_F2,
-        "\033[[C"  => self::KEY_F3,
-        "\033[[D"  => self::KEY_F4,
-        "\033[[E"  => self::KEY_F5,
-        "\033[15~"  => self::KEY_F5,
-        "\033[17~"  => self::KEY_F6,
-        "\033[18~"  => self::KEY_F7,
-        "\033[19~"  => self::KEY_F8,
-        "\033[20~"  => self::KEY_F9,
-        "\033[21~"  => self::KEY_F10,
-        "\033[23~"  => self::KEY_F11,
-        "\033[24~"  => self::KEY_F12,
+        "[1~" => self::KEY_HOME,
+        "[2~" => self::KEY_INSERT,
+        "[3~" => self::KEY_DEL,
+        "[4~" => self::KEY_END,
+        "[5~" => self::KEY_PGUP,
+        "[6~" => self::KEY_PGDOWN,
+        "[[5~" => self::KEY_PGUP,
+        "[[6~" => self::KEY_PGDOWN,
+        "[7~" => self::KEY_HOME,
+        "[8~" => self::KEY_END,
+        "OP"  => self::KEY_F1,
+        "OQ"  => self::KEY_F2,
+        "OR"  => self::KEY_F3,
+        "OS"  => self::KEY_F4,
+        "[11~"  => self::KEY_F1,
+        "[12~"  => self::KEY_F2,
+        "[13~"  => self::KEY_F3,
+        "[14~"  => self::KEY_F4,
+        "[[A"  => self::KEY_F1,
+        "[[B"  => self::KEY_F2,
+        "[[C"  => self::KEY_F3,
+        "[[D"  => self::KEY_F4,
+        "[[E"  => self::KEY_F5,
+        "[15~"  => self::KEY_F5,
+        "[17~"  => self::KEY_F6,
+        "[18~"  => self::KEY_F7,
+        "[19~"  => self::KEY_F8,
+        "[20~"  => self::KEY_F9,
+        "[21~"  => self::KEY_F10,
+        "[23~"  => self::KEY_F11,
+        "[24~"  => self::KEY_F12,
     ];
 
     protected $input;
-    protected $baseEmitter;
     protected $parser;
     protected $sequencer;
     protected $close = false;
+    protected $buffer = [];
 
-    public function __construct(ReadableStreamInterface $input, EventEmitterInterface $baseEmitter)
+    public function __construct(ReadableStreamInterface $input)
     {
         $this->input = $input;
-        $this->baseEmitter = $baseEmitter;
 
-        $parser = new ControlCodeParser($this->input);
+        $this->input->on('data', [$this, 'handleData']);
+        $this->input->on('end', [$this, 'handleEnd']);
+        $this->input->on('error', [$this, 'handleError']);
+        $this->input->on('close', [$this, 'close']);
 
-        $parser->on('csi', [$this, 'decoder']);
-        $parser->on('osc', [$this, 'decoder']);
-        $parser->on('c1', [$this, 'decoder']);
-        $parser->on('c0', [$this, 'decoder']);
+        // $this->sequencer = new Utf8Sequencer($this);
+        // $this->sequencer->on('data', [$this, 'fallback']);
 
-        $this->sequencer = new Utf8Sequencer($parser);
-        $this->sequencer->on('data', [$this, 'fallback']);
-
-        // process all stream events (forwarded from input stream)
-        $this->sequencer->on('end', [$this, 'handleEnd']);
-        $this->sequencer->on('error', [$this, 'handleError']);
-        $this->sequencer->on('close', [$this, 'close']);
+        // // process all stream events (forwarded from input stream)
+        // $this->sequencer->on('end', [$this, 'handleEnd']);
+        // $this->sequencer->on('error', [$this, 'handleError']);
+        // $this->sequencer->on('close', [$this, 'close']);
     }
 
-    public function decoder($code)
+    public function handleData($data) : void
     {
-        if ($code === "\r") {
-            $code = "\n";
+        if ($this->isMouse($data)) {
+            return;
         }
 
-        $event = isset(self::CODES[$code]) ? self::CODES[$code] : null;
+        $this->buffer = [];
+        $wholeThing = new UnicodeString($data);
 
-        if ($this->baseEmitter && $this->baseEmitter->listeners('keypress')) {
-            $this->baseEmitter->emit('keypress', [$code, $event]);
+        foreach ($wholeThing->match('/' . self::FUNC_KEYCODE . '|' . self::META_KEYCODE . '|' . "\x1b/", PREG_OFFSET_CAPTURE) as $match) {
+            if ($match[0]) {
+                $this->buffer = [ ...$this->buffer, ...$wholeThing->slice(0, $match[1])->split('//', null, PREG_SPLIT_NO_EMPTY) ];
+                $this->buffer[] = new UnicodeString($match[0]);
+                $wholeThing = $wholeThing->slice($match[1] + (new UnicodeString($match[0]))->length());
+
+                if ($wholeThing->length() === 0) {
+                    break;
+                }
+            }
         }
 
-        $this->emit('keypress', [$code, $event]);
+        $this->buffer = [ ...$this->buffer, ...$wholeThing->split('//', null, PREG_SPLIT_NO_EMPTY)];
+
+        foreach ($this->buffer as $str) {
+            $key = [
+                'sequence' => $str,
+                'name' => null,
+                'ctrl' => false,
+                'meta' => false,
+                'shift' => false,
+            ];
+
+            if ($str->equalsTo("\r")) {
+                // carriage return
+                $key['name'] = 'return';
+            }
+            else if ($str->equalsTo("\n")) {
+                // enter
+                $key['name'] = 'enter';
+            }
+            else if ($str->equalsTo("\t")) {
+                // tab
+                $key['name'] = 'tab';
+            }
+            else if ($str->equalsTo("\b") || $str->equalsTo("\x7f") || $str->equalsTo("\x1b\x7f") || $str->equalsTo("\x1b\b")) {
+                // backspace or ctrl+h
+                $key['name'] = 'backspace';
+                $key['meta'] = $str->indexOf("\x1b") === 0;
+            }
+            else if ($str->equalsTo("\x1b") || $str->equalsTo("\x1b\x1b")) {
+                // escape key
+                $key['name'] = 'escape';
+                $key['meta'] = $str->length() === 2;
+            }
+            else if ($str->equalsTo(' ') || $str->equalsTo("\x1b ")) {
+                $key['name'] = 'space';
+                $key['meta'] = $str->length() === 2;
+            }
+            else if ($str->length() === 1 && $str->toString() <= "\x1a") {
+                // ctrl+letter
+                $key['name'] = chr($str->codePointsAt(0)[0] + mb_ord('a') - 1);
+                $key['ctrl'] = true;
+            }
+            else if ($str->length() === 1 && $str->toString() >= 'a' && $str->toString() <= 'z') {
+                // lowercase letter
+                $key['name'] = $str->toString();
+            }
+            else if ($str->length() === 1 && $str->toString() >= 'A' && $str->toString() <= 'Z') {
+                // shift+letter
+                $key['name'] = $str->lower();
+                $key['shift'] = true;
+            }
+            else {
+                $parts = $str->match('/^' . self::META_KEYCODE . '$/');
+
+                if ( ! empty($parts)) {
+                    $part = new UnicodeString($parts[1]);
+
+                    // meta+character key
+                    $key['name'] = $part->lower();
+                    $key['meta'] = true;
+                    $key['shift'] = ! empty($part->match('/^[A-Z]$/'));
+                }
+
+                $parts = $str->match('/^' . self::FUNC_KEYCODE . '/');
+
+                if ( ! empty($parts)) {
+                    $code = $parts[1]
+                        . $parts[2]
+                        . $parts[4]
+                        . $parts[9];
+
+                    $modifier = ($parts[3] || $parts[8] || 1) - 1;
+
+                    $key['ctrl'] = !!($modifier & 4);
+                    $key['meta'] = !!($modifier & 10);
+                    $key['shift'] = !!($modifier & 1);
+                    $key['code'] = $code;
+                    $key['name'] = isset(self::CODES[$code]) ? self::CODES[$code] : null;
+                }
+            }
+
+            $ch = ($str->length() === 1) ? $str->toString() : null;
+
+            if ($key['name'] || $ch) {
+                $this->emit('keypress', [$ch, $key]);
+            }
+        }
     }
 
-    public function fallback($data)
+    public function isMouse($data) : bool
     {
-        $str = new UnicodeString($data);
-        $buffer = '';
-
-        foreach ($str->split('//', null, PREG_SPLIT_NO_EMPTY) as $char)
-        {
-            $strChar = $char->toString();
-
-            if ($this->baseEmitter && $this->baseEmitter->listeners('keypress'))
-            {
-                $this->baseEmitter->emit('keypress', [$char, null]);
-            }
-            else if ($this->listeners('keypress'))
-            {
-                $this->emit('keypress', [$char, null]);
-            }
-            else
-            {
-                $buffer .= $char;
+        foreach(self::IS_MOUSE as $match) {
+            if ( ! empty((new UnicodeString($data))->match($match))) {
+                return true;
             }
         }
 
-        if ($buffer) {
-            $this->emit('data', [$buffer]);
-        }
+        return false;
     }
 
     /** @internal */
-    public function handleEnd()
+    public function handleEnd() : void
     {
         if (! $this->closed) {
             $this->emit('end');
@@ -176,13 +262,13 @@ class Keys extends EventEmitter
     }
 
     /** @internal */
-    public function handleError(\Exception $error)
+    public function handleError(\Exception $error) : void
     {
         $this->emit('error', [$error]);
         $this->close();
     }
 
-    public function close()
+    public function close() : void
     {
         if ($this->closed) {
             return;
